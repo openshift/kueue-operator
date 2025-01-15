kannon92/openshift-kueue-operator

wip: trying to follow secondary scheduler operator

vendor/github.com/openshift/client-go/config/applyconfigurations/config/v1/clusterversionspec.go
@@ -1,0 +1,91 @@
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	v1 "github.com/openshift/api/config/v1"
+)
+
+// ClusterVersionSpecApplyConfiguration represents a declarative configuration of the ClusterVersionSpec type for use
+// with apply.
+type ClusterVersionSpecApplyConfiguration struct {
+	ClusterID       *v1.ClusterID                                     `json:"clusterID,omitempty"`
+	DesiredUpdate   *UpdateApplyConfiguration                         `json:"desiredUpdate,omitempty"`
+	Upstream        *v1.URL                                           `json:"upstream,omitempty"`
+	Channel         *string                                           `json:"channel,omitempty"`
+	Capabilities    *ClusterVersionCapabilitiesSpecApplyConfiguration `json:"capabilities,omitempty"`
+	SignatureStores []SignatureStoreApplyConfiguration                `json:"signatureStores,omitempty"`
+	Overrides       []ComponentOverrideApplyConfiguration             `json:"overrides,omitempty"`
+}
+
+// ClusterVersionSpecApplyConfiguration constructs a declarative configuration of the ClusterVersionSpec type for use with
+// apply.
+func ClusterVersionSpec() *ClusterVersionSpecApplyConfiguration {
+	return &ClusterVersionSpecApplyConfiguration{}
+}
+
+// WithClusterID sets the ClusterID field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the ClusterID field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithClusterID(value v1.ClusterID) *ClusterVersionSpecApplyConfiguration {
+	b.ClusterID = &value
+	return b
+}
+
+// WithDesiredUpdate sets the DesiredUpdate field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the DesiredUpdate field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithDesiredUpdate(value *UpdateApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	b.DesiredUpdate = value
+	return b
+}
+
+// WithUpstream sets the Upstream field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Upstream field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithUpstream(value v1.URL) *ClusterVersionSpecApplyConfiguration {
+	b.Upstream = &value
+	return b
+}
+
+// WithChannel sets the Channel field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Channel field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithChannel(value string) *ClusterVersionSpecApplyConfiguration {
+	b.Channel = &value
+	return b
+}
+
+// WithCapabilities sets the Capabilities field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Capabilities field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithCapabilities(value *ClusterVersionCapabilitiesSpecApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	b.Capabilities = value
+	return b
+}
+
+// WithSignatureStores adds the given value to the SignatureStores field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the SignatureStores field.
+func (b *ClusterVersionSpecApplyConfiguration) WithSignatureStores(values ...*SignatureStoreApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	for i := range values {
+		if values[i] == nil {
+			panic("nil value passed to WithSignatureStores")
+		}
+		b.SignatureStores = append(b.SignatureStores, *values[i])
+	}
+	return b
+}
+
+// WithOverrides adds the given value to the Overrides field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the Overrides field.
+func (b *ClusterVersionSpecApplyConfiguration) WithOverrides(values ...*ComponentOverrideApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	for i := range values {
+		if values[i] == nil {
+			panic("nil value passed to WithOverrides")
+		}
+		b.Overrides = append(b.Overrides, *values[i])
+	}
+	return b
+}

vendor/github.com/openshift/client-go/operator/applyconfigurations/operator/v1/dnsovertlsconfig.go
@@ -1,0 +1,36 @@
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	v1 "github.com/openshift/api/config/v1"
+)
+
+// DNSOverTLSConfigApplyConfiguration represents a declarative configuration of the DNSOverTLSConfig type for use
+// with apply.
+type DNSOverTLSConfigApplyConfiguration struct {
+	ServerName *string                    `json:"serverName,omitempty"`
+	CABundle   *v1.ConfigMapNameReference `json:"caBundle,omitempty"`
+}
+
+// DNSOverTLSConfigApplyConfiguration constructs a declarative configuration of the DNSOverTLSConfig type for use with
+// apply.
+func DNSOverTLSConfig() *DNSOverTLSConfigApplyConfiguration {
+	return &DNSOverTLSConfigApplyConfiguration{}
+}
+
+// WithServerName sets the ServerName field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the ServerName field is set to the value of the last call.
+func (b *DNSOverTLSConfigApplyConfiguration) WithServerName(value string) *DNSOverTLSConfigApplyConfiguration {
+	b.ServerName = &value
+	return b
+}
+
+// WithCABundle sets the CABundle field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the CABundle field is set to the value of the last call.
+func (b *DNSOverTLSConfigApplyConfiguration) WithCABundle(value v1.ConfigMapNameReference) *DNSOverTLSConfigApplyConfiguration {
+	b.CABundle = &value
+	return b
+}

vendor/github.com/openshift/client-go/operator/applyconfigurations/operator/v1/sflowconfig.go
@@ -1,0 +1,29 @@
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	v1 "github.com/openshift/api/operator/v1"
+)
+
+// SFlowConfigApplyConfiguration represents a declarative configuration of the SFlowConfig type for use
+// with apply.
+type SFlowConfigApplyConfiguration struct {
+	Collectors []v1.IPPort `json:"collectors,omitempty"`
+}
+
+// SFlowConfigApplyConfiguration constructs a declarative configuration of the SFlowConfig type for use with
+// apply.
+func SFlowConfig() *SFlowConfigApplyConfiguration {
+	return &SFlowConfigApplyConfiguration{}
+}
+
+// WithCollectors adds the given value to the Collectors field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the Collectors field.
+func (b *SFlowConfigApplyConfiguration) WithCollectors(values ...v1.IPPort) *SFlowConfigApplyConfiguration {
+	for i := range values {
+		b.Collectors = append(b.Collectors, values[i])
+	}
+	return b
+}

vendor/k8s.io/apiserver/pkg/storage/errors/doc.go
@@ -1,0 +1,18 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package storage provides conversion of storage errors to API errors.
+package storage // import "k8s.io/apiserver/pkg/storage/errors"

openshift/kueue-operator

vendor/github.com/openshift/client-go/config/applyconfigurations/config/v1/clusterversionspec.go
@@ -1,0 +1,91 @@
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	configv1 "github.com/openshift/api/config/v1"
+)
+
+// ClusterVersionSpecApplyConfiguration represents a declarative configuration of the ClusterVersionSpec type for use
+// with apply.
+type ClusterVersionSpecApplyConfiguration struct {
+	ClusterID       *configv1.ClusterID                               `json:"clusterID,omitempty"`
+	DesiredUpdate   *UpdateApplyConfiguration                         `json:"desiredUpdate,omitempty"`
+	Upstream        *configv1.URL                                     `json:"upstream,omitempty"`
+	Channel         *string                                           `json:"channel,omitempty"`
+	Capabilities    *ClusterVersionCapabilitiesSpecApplyConfiguration `json:"capabilities,omitempty"`
+	SignatureStores []SignatureStoreApplyConfiguration                `json:"signatureStores,omitempty"`
+	Overrides       []ComponentOverrideApplyConfiguration             `json:"overrides,omitempty"`
+}
+
+// ClusterVersionSpecApplyConfiguration constructs a declarative configuration of the ClusterVersionSpec type for use with
+// apply.
+func ClusterVersionSpec() *ClusterVersionSpecApplyConfiguration {
+	return &ClusterVersionSpecApplyConfiguration{}
+}
+
+// WithClusterID sets the ClusterID field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the ClusterID field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithClusterID(value configv1.ClusterID) *ClusterVersionSpecApplyConfiguration {
+	b.ClusterID = &value
+	return b
+}
+
+// WithDesiredUpdate sets the DesiredUpdate field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the DesiredUpdate field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithDesiredUpdate(value *UpdateApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	b.DesiredUpdate = value
+	return b
+}
+
+// WithUpstream sets the Upstream field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Upstream field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithUpstream(value configv1.URL) *ClusterVersionSpecApplyConfiguration {
+	b.Upstream = &value
+	return b
+}
+
+// WithChannel sets the Channel field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Channel field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithChannel(value string) *ClusterVersionSpecApplyConfiguration {
+	b.Channel = &value
+	return b
+}
+
+// WithCapabilities sets the Capabilities field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the Capabilities field is set to the value of the last call.
+func (b *ClusterVersionSpecApplyConfiguration) WithCapabilities(value *ClusterVersionCapabilitiesSpecApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	b.Capabilities = value
+	return b
+}
+
+// WithSignatureStores adds the given value to the SignatureStores field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the SignatureStores field.
+func (b *ClusterVersionSpecApplyConfiguration) WithSignatureStores(values ...*SignatureStoreApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	for i := range values {
+		if values[i] == nil {
+			panic("nil value passed to WithSignatureStores")
+		}
+		b.SignatureStores = append(b.SignatureStores, *values[i])
+	}
+	return b
+}
+
+// WithOverrides adds the given value to the Overrides field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the Overrides field.
+func (b *ClusterVersionSpecApplyConfiguration) WithOverrides(values ...*ComponentOverrideApplyConfiguration) *ClusterVersionSpecApplyConfiguration {
+	for i := range values {
+		if values[i] == nil {
+			panic("nil value passed to WithOverrides")
+		}
+		b.Overrides = append(b.Overrides, *values[i])
+	}
+	return b
+}

vendor/github.com/openshift/client-go/operator/applyconfigurations/operator/v1/dnsovertlsconfig.go
@@ -1,0 +1,36 @@
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	configv1 "github.com/openshift/api/config/v1"
+)
+
+// DNSOverTLSConfigApplyConfiguration represents a declarative configuration of the DNSOverTLSConfig type for use
+// with apply.
+type DNSOverTLSConfigApplyConfiguration struct {
+	ServerName *string                          `json:"serverName,omitempty"`
+	CABundle   *configv1.ConfigMapNameReference `json:"caBundle,omitempty"`
+}
+
+// DNSOverTLSConfigApplyConfiguration constructs a declarative configuration of the DNSOverTLSConfig type for use with
+// apply.
+func DNSOverTLSConfig() *DNSOverTLSConfigApplyConfiguration {
+	return &DNSOverTLSConfigApplyConfiguration{}
+}
+
+// WithServerName sets the ServerName field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the ServerName field is set to the value of the last call.
+func (b *DNSOverTLSConfigApplyConfiguration) WithServerName(value string) *DNSOverTLSConfigApplyConfiguration {
+	b.ServerName = &value
+	return b
+}
+
+// WithCABundle sets the CABundle field in the declarative configuration to the given value
+// and returns the receiver, so that objects can be built by chaining "With" function invocations.
+// If called multiple times, the CABundle field is set to the value of the last call.
+func (b *DNSOverTLSConfigApplyConfiguration) WithCABundle(value configv1.ConfigMapNameReference) *DNSOverTLSConfigApplyConfiguration {
+	b.CABundle = &value
+	return b
+}

vendor/github.com/openshift/client-go/operator/applyconfigurations/operator/v1/sflowconfig.go
@@ -1,0 +1,29 @@
+// Code generated by applyconfiguration-gen. DO NOT EDIT.
+
+package v1
+
+import (
+	operatorv1 "github.com/openshift/api/operator/v1"
+)
+
+// SFlowConfigApplyConfiguration represents a declarative configuration of the SFlowConfig type for use
+// with apply.
+type SFlowConfigApplyConfiguration struct {
+	Collectors []operatorv1.IPPort `json:"collectors,omitempty"`
+}
+
+// SFlowConfigApplyConfiguration constructs a declarative configuration of the SFlowConfig type for use with
+// apply.
+func SFlowConfig() *SFlowConfigApplyConfiguration {
+	return &SFlowConfigApplyConfiguration{}
+}
+
+// WithCollectors adds the given value to the Collectors field in the declarative configuration
+// and returns the receiver, so that objects can be build by chaining "With" function invocations.
+// If called multiple times, values provided by each call will be appended to the Collectors field.
+func (b *SFlowConfigApplyConfiguration) WithCollectors(values ...operatorv1.IPPort) *SFlowConfigApplyConfiguration {
+	for i := range values {
+		b.Collectors = append(b.Collectors, values[i])
+	}
+	return b
+}

vendor/k8s.io/apiserver/pkg/storage/errors/doc.go
@@ -1,0 +1,18 @@
+/*
+Copyright 2014 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Package storage provides conversion of storage errors to API errors.
+package errors // import "k8s.io/apiserver/pkg/storage/errors"