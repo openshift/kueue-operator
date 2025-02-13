2c7080bd4b182653a369d9a3a34ec1eaf6ed87ca
Pannaga Rao Bhoja Ramamanohara <pbhojara@pbhojara-thinkpadp16vgen1.rht.csb>
2025-02-13 12:01:48 -0500

    undeploy.sh:Update operator undeploy steps
    Signed-off-by: Pannaga Rao Bhoja Ramamanohara

--- hack/undeploy-ocp.sh
+++ hack/undeploy-ocp.sh
@@ -6,3 +6,6 @@ oc delete -f deploy/
 # this is to a fix bug
 oc delete mutatingwebhookconfigurations.admissionregistration.k8s.io kueue-mutating-webhook-configuration
 oc delete validatingwebhookconfigurations.admissionregistration.k8s.io kueue-validating-webhook-configuration
+oc get crds | grep kueue | awk '{print $1}' | xargs oc delete crd
+oc delete -f hack/manifests/cert-manager-rh.yaml
+oc delete namespaces cert-manager
