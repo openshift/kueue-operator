2c4ba3b830a2e02e3d53e463c2221703506e1ee9
Ryan Phillips <rphillips@redhat.com>
2025-03-28 15:45:42 -0500

    use bash from env

--- hack/revert-deploy-files.sh
+++ hack/revert-deploy-files.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Set back deploy scripts to original values
 
 OPERATOR_IMAGE_REPLACE=$1
 KUEUE_IMAGE_REPLACE=$2
-hack/replace-image.sh deploy $OPERATOR_IMAGE_REPLACE OPERATOR_IMAGE 
+hack/replace-image.sh deploy $OPERATOR_IMAGE_REPLACE OPERATOR_IMAGE
 hack/replace-image.sh deploy $KUEUE_IMAGE_REPLACE KUEUE_IMAGE
 hack/replace-image.sh deploy/examples $KUEUE_IMAGE_REPLACE KUEUE_IMAGE
