29092af4e84423e95d8aee74f36abd73451c687e
Ryan Phillips <rphillips@redhat.com>
2025-11-20 13:54:34 -0600

    restyle shell scripts with shellcheck

--- upstream/kueue/e2e-test-ocp.sh
+++ upstream/kueue/e2e-test-ocp.sh
@@ -18,59 +18,59 @@ export E2E_KIND_VERSION=""
 
 # To label worker nodes for e2e tests.
 function label_worker_nodes() {
-    echo "Labeling two worker nodes for e2e tests..."
-    # Retrieve the names of nodes with the "worker" role.
-    local nodes
-    IFS=' ' read -r -a nodes <<< "$($OC get nodes -l node-role.kubernetes.io/worker -o jsonpath='{.items[*].metadata.name}')"
-    
-    if [ ${#nodes[@]} -lt 2 ]; then
-        echo "Error: Found less than 2 worker nodes. Cannot assign labels."
-        exit 1
-    fi
-    
-    # Label the first node as "on-demand"
-    $OC label node "${nodes[0]}" instance-type=on-demand --overwrite
-    # Label the second node as "spot"
-    $OC label node "${nodes[1]}" instance-type=spot --overwrite
-    echo "Labeled ${nodes[0]} as on-demand and ${nodes[1]} as spot."
+  echo "Labeling two worker nodes for e2e tests..."
+  # Retrieve the names of nodes with the "worker" role.
+  local nodes
+  IFS=' ' read -r -a nodes <<< "$($OC get nodes -l node-role.kubernetes.io/worker -o jsonpath='{.items[*].metadata.name}')"
+
+  if [ ${#nodes[@]} -lt 2 ]; then
+    echo "Error: Found less than 2 worker nodes. Cannot assign labels."
+    exit 1
+  fi
+
+  # Label the first node as "on-demand"
+  $OC label node "${nodes[0]}" instance-type=on-demand --overwrite
+  # Label the second node as "spot"
+  $OC label node "${nodes[1]}" instance-type=spot --overwrite
+  echo "Labeled ${nodes[0]} as on-demand and ${nodes[1]} as spot."
 }
 
 function allow_privileged_access {
-    $OC adm policy add-scc-to-group privileged system:authenticated system:serviceaccounts
-    $OC adm policy add-scc-to-group anyuid system:authenticated system:serviceaccounts
+  $OC adm policy add-scc-to-group privileged system:authenticated system:serviceaccounts
+  $OC adm policy add-scc-to-group anyuid system:authenticated system:serviceaccounts
 }
 
 skips=(
-        # do not deploy AppWrapper in OCP
-        AppWrapper
-        # do not deploy PyTorch in OCP
-        PyTorch
-        # do not deploy JobSet in OCP
-        TrainJob
-        # do not deploy LWS in OCP
-        JAX
-        # do not deploy KubeRay in OCP
-        Kuberay
-        # metrics setup is different than our OCP setup
-        Metrics
-        # ring -> we do not enable Fair sharing by default in our operator
-        Fair
-        # we do not enable this feature in our operator
-        TopologyAwareScheduling
-        # For tests that rely on CPU setup, we need to fix upstream to get cpu allocatables from node
-        # rather than hardcoding CPU limits.
-        # relies on particular CPU setup to force pods to not schedule
-        "Failed Pod can be replaced in group"
-        "should allow to schedule a group of diverse pods"
-        "StatefulSet created with WorkloadPriorityClass"
-        "LeaderWorkerSet created with WorkloadPriorityClass"
-        "Pod groups when Single CQ"
-        # We do not have kueuectl in our operator
-        "Kueuectl"
+  # do not deploy AppWrapper in OCP
+  AppWrapper
+  # do not deploy PyTorch in OCP
+  PyTorch
+  # do not deploy JobSet in OCP
+  TrainJob
+  # do not deploy LWS in OCP
+  JAX
+  # do not deploy KubeRay in OCP
+  Kuberay
+  # metrics setup is different than our OCP setup
+  Metrics
+  # ring -> we do not enable Fair sharing by default in our operator
+  Fair
+  # we do not enable this feature in our operator
+  TopologyAwareScheduling
+  # For tests that rely on CPU setup, we need to fix upstream to get cpu allocatables from node
+  # rather than hardcoding CPU limits.
+  # relies on particular CPU setup to force pods to not schedule
+  "Failed Pod can be replaced in group"
+  "should allow to schedule a group of diverse pods"
+  "StatefulSet created with WorkloadPriorityClass"
+  "LeaderWorkerSet created with WorkloadPriorityClass"
+  "Pod groups when Single CQ"
+  # We do not have kueuectl in our operator
+  "Kueuectl"
 )
 skipsRegex=$(
-        IFS="|"
-        printf "%s" "${skips[*]}"
+  IFS="|"
+  printf "%s" "${skips[*]}"
 )
 
 GINKGO_SKIP_PATTERN="($skipsRegex)"
--- upstream/kueue/utils.sh
+++ upstream/kueue/utils.sh
@@ -2,19 +2,19 @@
 # shellcheck disable=SC2164
 
 apply_patches() {
-	for patch in patch/*.patch; do
-		pushd src >/dev/null
-		# Check if patch can be applied (not already applied)
-		if git apply --check ../"$patch" 2>/dev/null; then
-			echo "Applying $patch"
-			git apply ../"$patch" || {
-				echo "Error: Failed to apply $patch"
-				popd >/dev/null
-				return 1
-			}
-		else
-			echo "Skipping $patch (already applied or conflicts)"
-		fi
-		popd >/dev/null
-	done
+  for patch in patch/*.patch; do
+    pushd src >/dev/null
+    # Check if patch can be applied (not already applied)
+    if git apply --check ../"$patch" 2>/dev/null; then
+      echo "Applying $patch"
+      git apply ../"$patch" || {
+        echo "Error: Failed to apply $patch"
+        popd >/dev/null
+        return 1
+      }
+    else
+      echo "Skipping $patch (already applied or conflicts)"
+    fi
+    popd >/dev/null
+  done
 }
