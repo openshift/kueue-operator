2438f3475371caeadbb6e1fa64169789eb9a39ea
Sohan Kunkerkar <sohank2602@gmail.com>
2025-12-05 14:24:43 -0500

    configmap: add DRA configuration mapping

--- pkg/configmap/configmap_test.go
+++ pkg/configmap/configmap_test.go
@@ -336,6 +336,76 @@ webhook:
 			},
 			wantErr: nil,
 		},
+		"dra with device class mappings": {
+			configuration: kueue.KueueConfiguration{
+				Integrations: kueue.Integrations{
+					Frameworks: []kueue.KueueIntegration{kueue.KueueIntegrationBatchJob},
+				},
+				Resources: kueue.Resources{
+					DeviceClassMappings: []kueue.DeviceClassMapping{
+						{
+							Name:             "example.com/gpus",
+							DeviceClassNames: []string{"gpu.example.com", "gpu-large.example.com"},
+						},
+					},
+				},
+			},
+			wantCfgMap: &corev1.ConfigMap{
+				Data: map[string]string{
+					"controller_manager_config.yaml": `apiVersion: config.kueue.x-k8s.io/v1beta1
+clientConnection:
+  burst: 100
+  qps: 50
+controller:
+  groupKindConcurrency:
+    ClusterQueue.kueue.x-k8s.io: 1
+    Job.batch: 5
+    LocalQueue.kueue.x-k8s.io: 1
+    Pod: 5
+    ResourceFlavor.kueue.x-k8s.io: 1
+    Workload.kueue.x-k8s.io: 5
+fairSharing:
+  enable: false
+featureGates:
+  DynamicResourceAllocation: true
+health:
+  healthProbeBindAddress: :8081
+integrations:
+  frameworks:
+  - batch/job
+internalCertManagement:
+  enable: false
+kind: Configuration
+leaderElection:
+  leaderElect: true
+  leaseDuration: 2m17s
+  renewDeadline: 1m47s
+  resourceLock: ""
+  resourceName: ""
+  resourceNamespace: ""
+  retryPeriod: 26s
+manageJobsWithoutQueueName: false
+managedJobsNamespaceSelector:
+  matchLabels:
+    kueue.openshift.io/managed: "true"
+metrics:
+  bindAddress: :8443
+  enableClusterQueueResources: true
+namespace: test
+resources:
+  deviceClassMappings:
+  - deviceClassNames:
+    - gpu.example.com
+    - gpu-large.example.com
+    name: example.com/gpus
+waitForPodsReady: {}
+webhook:
+  port: 9443
+`,
+				},
+			},
+			wantErr: nil,
+		},
 	}
 
 	for desc, tc := range testCases {
